a9da49cc7a6a14915e99e1fb41ec2570b0224bd2
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 12:24:59 +0530

    Update JwtService.java

--- src/main/java/com/car_rental/car_rental_system/service/JwtService.java
+++ src/main/java/com/car_rental/car_rental_system/service/JwtService.java
@@ -1,8 +1,16 @@
 package com.car_rental.car_rental_system.service;
 
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+
 /**
  * @author Dilan
  * @created 27/02/2024 - 12:20 pm
  */
 public interface JwtService {
+    String generateToken(String username);
+    String extractUsername(String token);
+    Boolean validateToken(String token, UserDetails userDetails);
 }
