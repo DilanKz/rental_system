4fdbf19b7a5bbbd59c9f2b8fd80119faff917764
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 08:55:02 +0530

    Update CarRentalSystemApplication.java

--- src/main/java/com/car_rental/car_rental_system/CarRentalSystemApplication.java
+++ src/main/java/com/car_rental/car_rental_system/CarRentalSystemApplication.java
@@ -1,13 +1,26 @@
 package com.car_rental.car_rental_system;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CarRentalSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarRentalSystemApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
     }
 
 }
