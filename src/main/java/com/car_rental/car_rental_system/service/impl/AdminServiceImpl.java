15eef66246423ffcc1c17f78ea29576a4f9323e3
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 12:45:47 +0530

    Create AdminServiceImpl.java

---
+++ src/main/java/com/car_rental/car_rental_system/service/impl/AdminServiceImpl.java
@@ -0,0 +1,16 @@
+package com.car_rental.car_rental_system.service.impl;
+
+import com.car_rental.car_rental_system.dto.AdminDTO;
+import com.car_rental.car_rental_system.service.AdminService;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 12:44 pm
+ */
+public class AdminServiceImpl implements AdminService {
+
+    @Override
+    public AdminDTO findByUsername(String username) {
+        return null;
+    }
+}
