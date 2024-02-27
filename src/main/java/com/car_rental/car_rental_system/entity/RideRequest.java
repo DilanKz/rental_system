46d5805aaaee47d7dc77e5d8b3cf181cd386765d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 10:32:22 +0530

    Create RideRequest.java

---
+++ src/main/java/com/car_rental/car_rental_system/entity/RideRequest.java
@@ -0,0 +1,49 @@
+package com.car_rental.car_rental_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 10:21 am
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "ride_request")
+public class RideRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "req_no")
+    private int reqNo;
+
+    @Column(name = "vehicle_model")
+    private String name;
+
+    @Column(name = "req_dates")
+    private String date;
+
+    @Column(name = "pickup_location")
+    private String pickupLocation;
+
+    @Column(name = "destination")
+    private String destination;
+
+    @Column(name = "status")
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "car_id")
+    private Car car;
+
+    @ManyToOne
+    @JoinColumn(name = "uid", referencedColumnName = "uid")
+    private User user;
+}
