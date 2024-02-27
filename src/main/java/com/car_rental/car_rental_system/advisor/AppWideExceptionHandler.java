6066b6b4c3991913a8926e41ed190b0d76d2254a
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 11:56:20 +0530

    Update AppWideExceptionHandler.java

--- src/main/java/com/car_rental/car_rental_system/advisor/AppWideExceptionHandler.java
+++ src/main/java/com/car_rental/car_rental_system/advisor/AppWideExceptionHandler.java
@@ -3,6 +3,7 @@ package com.car_rental.car_rental_system.advisor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,13 +18,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppWideExceptionHandler {
 
     @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity handleException(RuntimeException e) {
+    public ResponseEntity handleServerException(RuntimeException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity handleAuthException(AccessDeniedException e){
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class})
+    public ResponseEntity handleCredentialsException(UsernameNotFoundException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 }
