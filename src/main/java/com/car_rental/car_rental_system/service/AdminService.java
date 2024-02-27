80315837801ddeeb6ad6d5a1ccb47fad99a622ea
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 13:41:47 +0530

    Update AdminService.java

--- src/main/java/com/car_rental/car_rental_system/service/AdminService.java
+++ src/main/java/com/car_rental/car_rental_system/service/AdminService.java
@@ -1,11 +1,13 @@
 package com.car_rental.car_rental_system.service;
 
 import com.car_rental.car_rental_system.dto.AdminDTO;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Dilan
  * @created 27/02/2024 - 12:26 pm
  */
+@Service
 public interface AdminService {
     AdminDTO findByUsername(String username);
 }
