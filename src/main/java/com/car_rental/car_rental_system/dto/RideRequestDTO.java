e84bfb2ca21d61ed96ab7b01a472934c63260eb1
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 17:47:34 +0530

    Create RideRequestDTO.java

---
+++ src/main/java/com/car_rental/car_rental_system/dto/RideRequestDTO.java
@@ -0,0 +1,36 @@
+package com.car_rental.car_rental_system.dto;
+
+import com.car_rental.car_rental_system.entity.User;
+import com.car_rental.car_rental_system.entity.Vehicle;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 05:44 pm
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class RideRequestDTO {
+
+    private int reqNo;
+
+    private String name;
+
+    private String date;
+
+    private String pickupLocation;
+
+    private String destination;
+
+    private String status;
+
+    private Vehicle car;
+
+    private User user;
+}
