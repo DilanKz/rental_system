12273f4b0138a162700b3e739eed86573ca4556f
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 13:33:34 +0530

    Update UserServiceImpl.java

--- src/main/java/com/car_rental/car_rental_system/service/impl/UserServiceImpl.java
+++ src/main/java/com/car_rental/car_rental_system/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.car_rental.car_rental_system.service.impl;
 
 import com.car_rental.car_rental_system.dto.UserDTO;
+import com.car_rental.car_rental_system.entity.User;
+import com.car_rental.car_rental_system.repo.UserRepository;
 import com.car_rental.car_rental_system.service.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,33 +14,64 @@ import java.util.List;
  */
 public class UserServiceImpl implements UserService {
 
+    private UserRepository userRepository;
+
     @Override
     public List<UserDTO> findAll() {
-        return null;
+        List<User> users = userRepository.findAll();
+        List<UserDTO> userDTOS = new ArrayList<>();
+
+        for (User user : users) {
+            userDTOS.add(new UserDTO(user.getUid(), user.getName(), user.getEmail(), user.getUsername(), user.getPassword()));
+        }
+
+        return userDTOS;
     }
 
     @Override
     public void save(UserDTO userDTO) {
+        UserDTO dto = findByUsername(userDTO.getUsername());
+
+        if (dto != null) {
+            return;
+        }
+
+        userRepository.save(new User(0, userDTO.getName(), userDTO.getEmail(), userDTO.getUsername(), userDTO.getPassword()));
 
     }
 
     @Override
     public void update(UserDTO userDTO) {
+        UserDTO dto = findByUsername(userDTO.getUsername());
 
+        if (dto == null) {
+            return;
+        }
+
+        userRepository.save(new User(userDTO.getUid(), userDTO.getName(), userDTO.getEmail(), userDTO.getUsername(), userDTO.getPassword()));
     }
 
     @Override
     public UserDTO findById(int id) {
-        return null;
+
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null) return null;
+
+        return new UserDTO(user.getUid(), user.getName(), user.getEmail(), user.getUsername(), user.getPassword());
     }
 
     @Override
     public UserDTO findByUsername(String username) {
-        return null;
+        User user = userRepository.findByUsername(username).orElse(null);
+
+        if (user == null) return null;
+
+        return new UserDTO(user.getUid(), user.getName(), user.getEmail(), user.getUsername(), user.getPassword());
     }
 
     @Override
     public void delete(int id) {
-
+        userRepository.deleteById(id);
     }
 }
