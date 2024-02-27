52e03f6ee3f733c6f90c2d0c10b605755023d34e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 12:20:45 +0530

    Update JwtService.java

--- src/main/java/com/car_rental/car_rental_system/service/JwtService.java
+++ src/main/java/com/car_rental/car_rental_system/service/JwtService.java
@@ -1,71 +1,8 @@
 package com.car_rental.car_rental_system.service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-
-import java.security.Key;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-
 /**
  * @author Dilan
- * @created 27/02/2024 - 08:31 am
+ * @created 27/02/2024 - 12:20 pm
  */
-
-@Service
-public class JwtService {
-
-    private final String SECRET_KEY = "3A1F79A64C0B8E9CF29A670BBD8F5A436A3D344B2A683F0A3B2507E53BB1CD07";
-    private static final int MINUTES = 60;
-
-    public String generateToken(String username) {
-        Instant now = Instant.now();
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(Date.from(now))
-                .setExpiration(Date.from(now.plus(MINUTES, ChronoUnit.MINUTES)))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public String extractUsername(String token) {
-        return getTokenBody(token).getSubject();
-    }
-
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
-    }
-
-    private Claims getTokenBody(String token) {
-        try {
-            return Jwts
-                    .parserBuilder()
-                    .setSigningKey(getSignInKey())
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (ExpiredJwtException e) {
-            throw new AccessDeniedException("Access denied: " + e.getMessage());
-        }
-    }
-
-    private boolean isTokenExpired(String token) {
-        Claims claims = getTokenBody(token);
-        return claims.getExpiration().before(new Date());
-    }
-
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
+public interface JwtService {
 }
