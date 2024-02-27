f2c97ececc1e1e8f1417c1849038be66a53dab92
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 11:44:55 +0530

    Update UserRepository.java

--- src/main/java/com/car_rental/car_rental_system/repo/UserRepository.java
+++ src/main/java/com/car_rental/car_rental_system/repo/UserRepository.java
@@ -3,9 +3,12 @@ package com.car_rental.car_rental_system.repo;
 import com.car_rental.car_rental_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author Dilan
  * @created 27/02/2024 - 10:34 am
  */
 public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername (String username);
 }
