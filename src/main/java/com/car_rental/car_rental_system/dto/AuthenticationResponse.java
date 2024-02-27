d95f980e361a8a28bfc3eb9fb2c420757791cf42
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 14:00:52 +0530

    Create AuthenticationResponse.java

---
+++ src/main/java/com/car_rental/car_rental_system/dto/AuthenticationResponse.java
@@ -0,0 +1,20 @@
+package com.car_rental.car_rental_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 02:00 pm
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class AuthenticationResponse {
+    private String token;
+    private String email;
+}
