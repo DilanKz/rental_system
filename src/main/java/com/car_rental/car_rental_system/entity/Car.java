a7bd070decd6f69acfe64481d55b11e84e004ab1
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 10:32:15 +0530

    Create Car.java

---
+++ src/main/java/com/car_rental/car_rental_system/entity/Car.java
@@ -0,0 +1,43 @@
+package com.car_rental.car_rental_system.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 10:17 am
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_id")
+    private int carId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "plate_number")
+    private String plateNumber;
+
+    @ElementCollection
+    @Column(name = "req_dates")
+    private List<Date> reqDates;
+
+}
