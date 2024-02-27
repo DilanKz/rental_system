a1b76e5cd7e4f9ad6d4fbedfbd68ab6a903aeaf8
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 08:31:37 +0530

    added jwt and created jwt , security files

---
+++ src/main/java/com/car_rental/car_rental_system/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.car_rental.car_rental_system.config;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 08:30 am
+ */
+public class SecurityConfig {
+}
---
+++ src/main/java/com/car_rental/car_rental_system/service/JwtService.java
@@ -0,0 +1,8 @@
+package com.car_rental.car_rental_system.service;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 08:31 am
+ */
+public class JwtService {
+}
