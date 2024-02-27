1ea51503e35c336f093a36005b019f98d05597fa
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 21:58:37 +0530

    Update RideRequestService.java

--- src/main/java/com/car_rental/car_rental_system/service/RideRequestService.java
+++ src/main/java/com/car_rental/car_rental_system/service/RideRequestService.java
@@ -1,8 +1,20 @@
 package com.car_rental.car_rental_system.service;
 
+import com.car_rental.car_rental_system.dto.RideRequestDTO;
+
+import java.util.List;
+
 /**
  * @author Dilan
  * @created 27/02/2024 - 08:56 pm
  */
 public interface RideRequestService {
+
+    List<RideRequestDTO> findAll();
+
+    RideRequestDTO findById(int id);
+
+    void save(RideRequestDTO dto);
+
+    void update(RideRequestDTO dto);
 }
