54e814aff69bf2d06e949b84cac654f4ce8d2af0
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 11:44:52 +0530

    Update AdminRepository.java

--- src/main/java/com/car_rental/car_rental_system/repo/AdminRepository.java
+++ src/main/java/com/car_rental/car_rental_system/repo/AdminRepository.java
@@ -3,9 +3,12 @@ package com.car_rental.car_rental_system.repo;
 import com.car_rental.car_rental_system.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author Dilan
  * @created 27/02/2024 - 10:33 am
  */
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
+    Optional<Admin> findByUsername (String username);
 }
