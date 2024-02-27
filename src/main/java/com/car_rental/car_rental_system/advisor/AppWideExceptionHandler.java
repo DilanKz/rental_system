73c35221d2d7252af23b3ea7093c4cc8a905211b
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 09:40:20 +0530

    Create AppWideExceptionHandler.java

---
+++ src/main/java/com/car_rental/car_rental_system/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.car_rental.car_rental_system.advisor;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 09:24 am
+ */
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+}
