6dab54fbc34f5a08a9984a6da27564081a833b23
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 12:45:49 +0530

    Create UserServiceImpl.java

---
+++ src/main/java/com/car_rental/car_rental_system/service/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.car_rental.car_rental_system.service.impl;
+
+import com.car_rental.car_rental_system.dto.UserDTO;
+import com.car_rental.car_rental_system.service.UserService;
+
+import java.util.List;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 12:45 pm
+ */
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<UserDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(UserDTO userDTO) {
+
+    }
+
+    @Override
+    public void update(UserDTO userDTO) {
+
+    }
+
+    @Override
+    public UserDTO findById(int id) {
+        return null;
+    }
+
+    @Override
+    public UserDTO findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
