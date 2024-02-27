0042ce06370d7eea9c15020da40b3f4b0cecc94e
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 14:10:52 +0530

    change password encrypt to NoOp

--- src/main/java/com/car_rental/car_rental_system/config/SecurityConfig.java
+++ src/main/java/com/car_rental/car_rental_system/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -102,9 +103,10 @@ public class SecurityConfig {
         };
     }
 
+    @SuppressWarnings("deprecation")
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
 }
