fb7059a256e8adb5dfe899817e06bb92aa875d4c
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 12:44:15 +0530

    Create AdminDTO.java

---
+++ src/main/java/com/car_rental/car_rental_system/dto/AdminDTO.java
@@ -0,0 +1,23 @@
+package com.car_rental.car_rental_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 12:33 pm
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AdminDTO {
+
+    private int id;
+
+    private String username;
+
+    private String password;
+}
