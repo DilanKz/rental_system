3dcbc472a2eeef3df3e926dfb7e7f7aa908c06c2
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 12:44:19 +0530

    Create UserDTO.java

---
+++ src/main/java/com/car_rental/car_rental_system/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.car_rental.car_rental_system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author Dilan
+ * @created 27/02/2024 - 12:27 pm
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserDTO {
+
+    private int uid;
+
+    private String name;
+
+    private String email;
+
+    private String username;
+
+    private String password;
+}
