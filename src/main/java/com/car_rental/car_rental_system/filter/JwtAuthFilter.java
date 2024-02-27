e8ad7520a80dc751a98d4cf7f1fa7d1f414e8f25
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-27 08:54:30 +0530

    Update JwtAuthFilter.java

--- src/main/java/com/car_rental/car_rental_system/filter/JwtAuthFilter.java
+++ src/main/java/com/car_rental/car_rental_system/filter/JwtAuthFilter.java
@@ -1,8 +1,76 @@
 package com.car_rental.car_rental_system.filter;
 
+import com.car_rental.car_rental_system.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
 /**
  * @author Dilan
  * @created 27/02/2024 - 08:30 am
  */
-public class JwtAuthFilter {
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    private ObjectMapper mapper;
+
+    public JwtAuthFilter(JwtService jwtService, ObjectMapper mapper) {
+        this.jwtService = jwtService;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String authHeader = request.getHeader("Authorization");
+            String token = null;
+            String username = null;
+
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username = jwtService.extractUsername(token);
+            }
+
+            //If the accessToken is null. It will pass the request to the next filter.
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            //If any accessToken is present, then it will validate the token and then authenticate the request in security context
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+                UserDetails userDetails = User.builder().username("Dilan").build();
+
+
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, null);
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+
+            filterChain.doFilter(request, response);
+
+        } catch (AccessDeniedException e) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+    }
 }
