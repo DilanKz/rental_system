8b09ddd3a8824c12ffb81ac26d0e4abf0e628d24
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-28 14:39:43 +0530

    Update RideRequestRepository.java

--- src/main/java/com/car_rental/car_rental_system/repo/RideRequestRepository.java
+++ src/main/java/com/car_rental/car_rental_system/repo/RideRequestRepository.java
@@ -1,6 +1,7 @@
 package com.car_rental.car_rental_system.repo;
 
 import com.car_rental.car_rental_system.entity.RideRequest;
+import com.car_rental.car_rental_system.entity.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +11,5 @@ import java.util.List;
  * @created 27/02/2024 - 10:36 am
  */
 public interface RideRequestRepository extends JpaRepository<RideRequest,Integer> {
-    List<RideRequest> findAllByStatus(String status);
+    List<RideRequest> findAllByStatus(RequestStatus status);
 }
