13d4a4ebd7b5a9d956b785df9eafa4c5b0483d1d
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-28 14:39:46 +0530

    Update VehicleRepository.java

--- src/main/java/com/car_rental/car_rental_system/repo/VehicleRepository.java
+++ src/main/java/com/car_rental/car_rental_system/repo/VehicleRepository.java
@@ -1,6 +1,7 @@
 package com.car_rental.car_rental_system.repo;
 
 import com.car_rental.car_rental_system.entity.Vehicle;
+import com.car_rental.car_rental_system.entity.enums.VehicleModels;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,6 +11,6 @@ import java.util.Optional;
  * @created 27/02/2024 - 10:36 am
  */
 public interface VehicleRepository extends JpaRepository<Vehicle,Integer> {
-    Optional<Vehicle> findByModel(String model);
+    Optional<Vehicle> findByModel(VehicleModels model);
     Optional<Vehicle> findByPlateNumber(String plateNumber);
 }
