c53369a622ffa271541fe3dce2781a7d838a3e1c
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-29 09:29:56 +0530

    Update RideRequestServiceImpl.java

--- src/main/java/com/car_rental/car_rental_system/service/impl/RideRequestServiceImpl.java
+++ src/main/java/com/car_rental/car_rental_system/service/impl/RideRequestServiceImpl.java
@@ -15,6 +15,7 @@ import com.car_rental.car_rental_system.service.RideRequestService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -135,9 +136,41 @@ public class RideRequestServiceImpl implements RideRequestService {
         repository.save(request);
     }
 
+    /**
+     * Retrieves ride requests based on the specified pickup location and destination.
+     *
+     * @param pickupLocation The pickup location details
+     * @param destination    The destination location details
+     * @return List of RideRequestDTO representing ride requests matching the specified pickup location and destination
+     */
     @Override
     public List<RideRequestDTO> findByPickupLocationAndDestination(LocationDetails pickupLocation, LocationDetails destination) {
         return rideListConverter(repository.findByPickupLocationAndDestination(pickupLocation,destination));
+    }
+
+    /**
+     * Retrieves ride requests filtered by the specified pickup date.
+     *
+     * @param date The pickup date to filter the ride requests
+     * @return List of RideRequestDTO representing ride requests with the specified pickup date
+     */
+    @Override
+    public List<RideRequestDTO> filterFromDate(LocalDate date) {
+        List<RideRequest> allByPickupDate = repository.findAllByPickupDate(date);
+        return rideListConverter(allByPickupDate);
+    }
+
+    /**
+     * Retrieves ride requests filtered by the specified pickup date range.
+     *
+     * @param startDate The start date of the pickup date range
+     * @param endDate   The end date of the pickup date range
+     * @return List of RideRequestDTO representing ride requests within the specified pickup date range
+     */
+    @Override
+    public List<RideRequestDTO> filterBetweenDate(LocalDate startDate,LocalDate endDate){
+        List<RideRequest> allByPickupDate = repository.findAllByPickupDateBetween(startDate,endDate);
+        return rideListConverter(allByPickupDate);
     }
 
     /**
@@ -162,19 +195,27 @@ public class RideRequestServiceImpl implements RideRequestService {
      * @param request The RideRequest entity to convert
      * @return RideRequestDTO representing the converted ride request
      */
-    private RideRequestDTO rideRequestConverter(RideRequest request){
-        return new RideRequestDTO(
-                request.getReqNo(),
-                request.getName(),
-                request.getPickupDate(),
-                request.getReturnDate(),
-                request.getPickupLocation(),
-                request.getDestination(),
-                request.getStatus(),
-                request.getVehicle().getVehicleId(),
-                request.getUser().getUid()
-        );
+    private RideRequestDTO rideRequestConverter(RideRequest request) {
+        RideRequestDTO rideRequestDTO = new RideRequestDTO();
+
+
+        Vehicle vehicle = request.getVehicle();
+        if (vehicle != null) {
+            rideRequestDTO.setVehicle(vehicle.getVehicleId());
+        }
+
+        rideRequestDTO.setReqNo(request.getReqNo());
+        rideRequestDTO.setModel(request.getModel());
+        rideRequestDTO.setPickupDate(request.getPickupDate());
+        rideRequestDTO.setReturnDate(request.getReturnDate());
+        rideRequestDTO.setPickupLocation(request.getPickupLocation());
+        rideRequestDTO.setDestination(request.getDestination());
+        rideRequestDTO.setStatus(request.getStatus());
+        rideRequestDTO.setUser(request.getUser().getUid());
+
+        return rideRequestDTO;
     }
+
 
     /**
      * Converts a RideRequestDTO to a RideRequest entity.
@@ -199,7 +240,7 @@ public class RideRequestServiceImpl implements RideRequestService {
 
         return new RideRequest(
                 dto.getReqNo(),
-                dto.getName(),
+                dto.getModel(),
                 dto.getPickupDate(),
                 dto.getReturnDate(),
                 dto.getPickupLocation(),
