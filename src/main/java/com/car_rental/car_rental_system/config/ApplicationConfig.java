4f8a6e97540bc599475a928338911913f7b2bbe2
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-02-29 09:29:10 +0530

    Update ApplicationConfig.java

--- src/main/java/com/car_rental/car_rental_system/config/ApplicationConfig.java
+++ src/main/java/com/car_rental/car_rental_system/config/ApplicationConfig.java
@@ -3,6 +3,8 @@ package com.car_rental.car_rental_system.config;
 import com.car_rental.car_rental_system.entity.Admin;
 import com.car_rental.car_rental_system.repo.AdminRepository;
 import com.car_rental.car_rental_system.repo.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
@@ -49,6 +51,13 @@ public class ApplicationConfig {
                         .build();
             }
         };
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 
 }
