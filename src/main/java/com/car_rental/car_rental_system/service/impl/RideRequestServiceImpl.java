55612bbc82247c7b882dde6a2fad547f7141d8ed
Tharindu Dilan <Dilanmallawarachchi555@gmail.com>
2024-03-04 20:36:00 +0530

    Update RideRequestServiceImpl.java

--- src/main/java/com/car_rental/car_rental_system/service/impl/RideRequestServiceImpl.java
+++ src/main/java/com/car_rental/car_rental_system/service/impl/RideRequestServiceImpl.java
@@ -37,10 +37,11 @@ public class RideRequestServiceImpl implements RideRequestService {
     private UserRepository userRepository;
     private SendMail sendMail;
 
-    public RideRequestServiceImpl(RideRequestRepository repository, VehicleRepository vehicleRepository, UserRepository userRepository) {
+    public RideRequestServiceImpl(RideRequestRepository repository, VehicleRepository vehicleRepository, UserRepository userRepository, SendMail sendMail) {
         this.repository = repository;
         this.vehicleRepository = vehicleRepository;
         this.userRepository = userRepository;
+        this.sendMail = sendMail;
     }
 
     /**
@@ -205,18 +206,6 @@ public class RideRequestServiceImpl implements RideRequestService {
             rideRequestDTO.setStatus(status);
             RideRequest request = rideRequestDTOConverter(rideRequestDTO);
             repository.save(request);
-
-
-            String userEmail = request.getUser().getEmail();
-            String plateNumber = request.getVehicle().getPlateNumber();
-            String carName = request.getVehicle().getName();
-
-            String emailText = "<p>Your request has been approved with the following vehicle:</p>" +
-                    "<p><strong>Car:</strong> " + carName + "</p>" +
-                    "<p><strong>Plate Number:</strong> " + plateNumber + "</p>";
-
-            sendMail.sendEmail(userEmail, emailText);
-
 
         } catch (Exception e) {
             log.error("Error occurred in RideRequestServiceImpl while updating ride request status: {}", e.getMessage());
